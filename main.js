74c1ed269656695669f867ba3100d2bb58cac005
Sabinaxon08 <sabdugaffarovasabina@gmaSabinaxon08il.com>
2024-07-29 12:28:43 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let bgcolor = prompt("BG COLOR")
+let textcolor = prompt("TEXT COLOR")
+
+let body = document.querySelector('body')
+
+body.style.background = bgcolor
+body.style.color = textcolor
+
